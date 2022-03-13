300542bb27be567e601ee05082eda080927e7605
miloske1992miloske <95969250+miloske1992miloske@users.noreply.github.com>
2022-03-13 19:24:16 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 
-var menuBtn = document.getElementById('menu-btn')
-var sideNav = document.getElementById('side-nav')
-var menu = document.getElementById('menu')
+const menuBtn = document.getElementById('menu-btn')
+const sideNav = document.getElementById('side-nav')
+const menu = document.getElementById('menu')
 
 sideNav.style.top = '-100%';
 
-menuBtn.onclick = function() {
+menuBtn.addEventListener('click', mobileMenu) 
+
+function mobileMenu() {
 	if (sideNav.style.top == '-100%') {
 		sideNav.style.top = '0';
 		menu.src = 'img/icon-close.svg'
